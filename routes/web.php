85c51a4d87b5b155a92dc32f4327146a0046bf7a
Navjot Kumar singh <navjotkumarsingh81@gmail.com>
2026-03-13 11:14:35 +0530

    new updates

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\controllers\UserController;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\controllers\UserController; 
 
 // Route::get('/user/{id?}', function ($id=0) {
 //     return view('user',['id'=>$id]);
 // });
 
 // New Test
-Route::get('/user/{id?}',[UserController::class, 'show']);
+// Route::get('/user/{id?}',[UserController::class, 'show']);
 
-Route::get('/welcome-student/{name?}', [UserController::class, 'welcomeStudent']);
-Route::get('/student-name/{name?}', [UserController::class, 'studentName']);
-Route::get('/course/{courseName?}', [UserController::class, 'course']);
+// Route::get('/welcome-student/{name?}', [UserController::class, 'welcomeStudent']);
+// Route::get('/student-name/{name?}', [UserController::class, 'studentName']);
+// Route::get('/course/{courseName?}', [UserController::class, 'course']);
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentController;
+
+// Route::get('/user/{id?}', [UserController::class, 'show']);
+// Route::get('/student/{name?}', [StudentController::class, 'show']);
+
+Route::prefix('admin')->group(function () {
+  Route::get('/dashboard', function () {
+    return "This is the admin dashboard";
+  });
+  Route::get('/users', function () {
+    return "This is the admin users page";
+  });
+});
